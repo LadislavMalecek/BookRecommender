f8cc7e5243c449bff912387d8eaac5fa5a6da5ac
Ladislav Malecek <malecek.ladislav@gmail.com>
2017-03-13 22:16:05 +0100

    Counter modified to support multithreading

--- DataManipulation/Counter.cs
+++ DataManipulation/Counter.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
 
-namespace BookRecommender.DataManipulation{
+namespace BookRecommender.DataManipulation
+{
 
-class Counter : IDisposable
+    class Counter : IDisposable
     {
         int count = 0;
         int max;
@@ -16,22 +18,45 @@ class Counter : IDisposable
             sW.Start();
         }
 
-        public void Update(){
-            count++;
-            System.Console.Write(this);
+        public void Update()
+        {
+            Interlocked.Increment(ref count);
+            if (count < 100)
+            {
+                System.Console.Write(this);
+            }
+            else
+            {
+                if (count % 100 == 0)
+                {
+                    System.Console.Write(this);
+                }
+            }
+        }
+        public void UpdateOnly()
+        {
+            Interlocked.Increment(ref count);
         }
 
         public void Dispose()
         {
             Console.WriteLine();
             sW.Stop();
-            var elapsed = ((double)sW.ElapsedMilliseconds)/1000;
+            var elapsed = ((double)sW.ElapsedMilliseconds) / 1000;
             System.Console.WriteLine($"Query execution time: {elapsed}s");
         }
 
         public override string ToString()
         {
-            return String.Format("\r{0}/{1}", count, max);
+            if (count != 0)
+            {
+                var eta = (int)(((double)sW.ElapsedMilliseconds) / count * (max - count));
+                return String.Format("\r{0}/{1}   ETA: {2}s", count, max, eta / 1000);
+            }
+            else
+            {
+                return String.Format("\r{0}/{1}   ETA: n\a", count, max);
+            }
         }
     }
 }
